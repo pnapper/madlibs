0cd85405b257a435f69820ba535b7f3eea969684
ld and pn <ld@>
2017-05-24 20:29:05 -0700

    add variables for person1, person2, animal, exclamation, verb and noun.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
   $("#blanks form").submit(function() {
-    $(".person1").text("blah blah");
-    $(".person2").text("blah blah");
-    $(".animal").text("blah blah");
-    $(".exclamation").text("blah blah");
-    $(".verb").text("blah blah");
-    $(".noun").text("blah blah");
+    var person1Input = $("input#person1").val();
+    var person2Input = $("input#person2").val();
+    var animalInput = $("input#animal").val();
+    var exclamationInput = $("input#exclamation").val();
+    var verbInput = $("input#verb").val();
+    var nounInput = $("input#noun").val();
+
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".animal").text(animalInput);
+    $(".exclamation").text(exclamationInput);
+    $(".verb").text(verbInput);
+    $(".noun").text(nounInput);
 
     $("#story").show();
 
